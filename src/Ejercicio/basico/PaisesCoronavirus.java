7db50839ebcaf2fa9087a96cda8c3b411f3ac7ab
agti <arieltammone@gmail.com>
2020-03-02 13:55:02 +0100

    cambio

--- src/Ejercicio/basico/PaisesCoronavirus.java
+++ src/Ejercicio/basico/PaisesCoronavirus.java
@@ -3,8 +3,30 @@ package Ejercicio.basico;
 public class PaisesCoronavirus {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		final String[] PAISES= {"Noruega","Alemania","Republica Checa","Italia"};
+		boolean[]paisesInfectados= {false,true,false,false};
+		int[] numeroinfectados= {0,5,0,0};
+		int i;
+		
+		
+		for (i=0; i<paisesInfectados.length;i++){
+		
+			if(paisesInfectados[i]==false) {
+				System.out.println(PAISES[i]+" no está infectado");
+			
+			} else {
+				System.out.println(PAISES[i]+ "está infectado");
+			
+			}			
+				
 
+		}
+		
+		for (i=0;i<paisesInfectados.length;i++) {
+			if(numeroinfectados[i]>0)
+			System.out.println("en " + PAISES[i]+ " hay "+numeroinfectados[i]+" infectados");
+		}
+		
+         
 	}
-
 }
